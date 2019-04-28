3442d73184e9f7c19d4e5bc0e9f3437d2eb90218
sudingquan <1151015256@qq.com>
2019-04-28 15:03:18 +0800

    modify 1290.cpp

--- 1290.cpp
+++ 1290.cpp
@@ -10,39 +10,26 @@
 
 using namespace std;
 
+int f(long long a, long long b, int s) {
+    if (a < b){
+        swap(a, b);
+    }
+    if (a / b > 1 || a % b == 0) {
+        return s;
+    }
+    return f(a - b, b ,(s + 1) % 2);
+}
+
 int main() {
-    int a, b, n;
+    int n;
+    long long a, b;
     scanf("%d", &n);
     while (n --) {
-        scanf("%d%d", &a, &b);
-        while(true) {
-            int x = 0;
-            if(a == b) {
-                printf("Stan wins\n");
-                break;
-            } else if (a > b) {
-                if (a % b == 0) {
-                    printf("Stan wins\n");
-                    break;
-                } else if (a / b > 1) {
-                    printf("Stan wins\n");
-                    break;
-                } else { 
-                    a -= b;
-                    x ++;
-                }
-            } else {
-                if (b % a == 0) {
-                    printf("Ollie wins\n");
-                    break;
-                } else if (b / a > 1) {
-                    printf("Ollie wins\n");
-                    break;
-                } else {
-                    b -= a;
-                    x ++;
-                }
-            }
+        scanf("%lld%lld", &a, &b);
+        if (f(a, b, 0) == 0) {
+            printf("Stan wins\n");
+        } else {
+            printf("Ollie wins\n");
         }
     }
     return 0;
